63b8ef0a865b2ec7a4c3a7b899d818002df2c2f5
Nya Feinstein <110700248+nyafein@users.noreply.github.com>
2024-09-06 17:24:05 -0400

    V3

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,10 @@ st.write("This app lets you enter a list of names and then randomly selects one.
 # Input for names from the user (each name separated by a comma)
 names_input = st.text_area("Enter names separated by commas:")
 
+# Input for text whose words need to be counted
+# user_input = st.text_area("Enter some text:")
+
+st.button("Let's go", type="primary")
 # Button to trigger random name selection and word count
 if st.button("Select Random Name"):
     # Split the names by commas and strip whitespace
@@ -20,7 +24,7 @@ if st.button("Select Random Name"):
     if names_list:
         # Select a random name from the list
         selected_name = random.choice(names_list)
-    
+        
         
         # Display the random name and word count
         st.write(f"Randomly Selected Name: {selected_name}")
